a5a91dfdb51148990e822563cf7f5c727ac2ef02
slowe <gravatar@strudel.org.uk>
2021-01-27 19:20:16 +0000

    Add click event

--- resources/vaccines.js
+++ resources/vaccines.js
@@ -40,6 +40,32 @@
 			tmp.body.innerHTML = str;
 			return tmp.body.children;
 		}
+		function highlight(e){
+			a = e.target.closest('svg').querySelector('.active');
+			if(a) a.classList.remove('active');
+			// Simulate z-index by moving target
+			e.target.closest('svg').appendChild(e.target);
+			e.target.classList.add('active');
+			
+			container = e.target.closest('.map');
+			tooltips = document.querySelectorAll('.tooltip');
+			if(tooltips){
+				for(i = 0 ; i < tooltips.length; i++) tooltips[i].parentNode.removeChild(tooltips[i]);
+			}
+			
+			data = e.target.closest('svg').getAttribute('data');
+			key = "data-"+data.replace(/ /g,"-").replace(/[^a-zA-Z0-9\-]/g,"").toLowerCase();
+			tooltip = document.createElement('div');
+			tooltip.id = "tooltip";
+			tooltip.innerHTML = e.target.getAttribute('data-stp20nm')+"<br />"+parseFloat(e.target.getAttribute(key)).toLocaleString()+(key.indexOf('pc')>0 ? '%' : '');
+			tooltip.classList.add('tooltip');
+			e.target.closest('div').appendChild(tooltip);
+			bb = e.target.getBoundingClientRect();
+			sv = e.target.closest('svg').getBoundingClientRect();
+			tooltip.style.position = "absolute";
+			tooltip.style.left = Math.round(bb.left+(bb.width/2)-sv.left)+'px';
+			tooltip.style.top = Math.round(bb.top+bb.height-sv.top)+'px';
+		}
 		var im = document.querySelectorAll('img.svg');
 		var img = new Array(im.length);
 		for(var i = 0; i < im.length; i++){
@@ -57,33 +83,8 @@
 					// Now add functions
 					paths = svg.querySelectorAll('path');
 					for(p = 0; p < paths.length; p++){
-						paths[p].addEventListener('mouseover', function(e){
-							a = e.target.closest('svg').querySelector('.active');
-							if(a) a.classList.remove('active');
-							// Simulate z-index by moving target
-							e.target.closest('svg').appendChild(e.target);
-							e.target.classList.add('active');
-							
-							container = e.target.closest('.map');
-							tooltips = document.querySelectorAll('.tooltip');
-							if(tooltips){
-								for(i = 0 ; i < tooltips.length; i++) tooltips[i].parentNode.removeChild(tooltips[i]);
-							}
-							
-							data = e.target.closest('svg').getAttribute('data');
-							key = "data-"+data.replace(/ /g,"-").replace(/[^a-zA-Z0-9\-]/g,"").toLowerCase();
-							tooltip = document.createElement('div');
-							tooltip.id = "tooltip";
-							tooltip.innerHTML = e.target.getAttribute('data-stp20nm')+"<br />"+parseFloat(e.target.getAttribute(key)).toLocaleString()+(key.indexOf('pc')>0 ? '%' : '');
-							tooltip.classList.add('tooltip');
-							e.target.closest('div').appendChild(tooltip);
-							bb = e.target.getBoundingClientRect();
-							sv = e.target.closest('svg').getBoundingClientRect();
-							tooltip.style.position = "absolute";
-							tooltip.style.left = Math.round(bb.left+(bb.width/2)-sv.left)+'px';
-							tooltip.style.top = Math.round(bb.top+bb.height-sv.top)+'px';
-							return true;
-						});
+						paths[p].addEventListener('mouseover', highlight);
+						paths[p].addEventListener('click', highlight);
 					}
 				}
 			});
